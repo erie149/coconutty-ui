20e6cbe5360569ffdb64a3dfc53ccba9c6a85694
root <root@localhost.localdomain>
2019-05-28 15:46:33 -0500

    update

--- src/buttercms.js
+++
@@ -1,6 +0,0 @@
-import Butter from 'buttercms'
-<<<<<<< HEAD
-export const butter = Butter('945964f13661b3d35528ab5bc7d1e6cc90a828e7')
-=======
-export const butter = Butter('')
->>>>>>> correctpackage
