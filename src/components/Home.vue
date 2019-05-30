9c883b4718c75241cfb3c948f36a39eed2250e2c
root <root@localhost.localdomain>
2019-05-30 10:27:46 -0500

    new search home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <div >
-    
-  </div>
+<div class="home-center-container">
+    <div class="home-center">
+        <md-content>Coconutty</md-content>
+    </div>
+    <div class="home-center">
+        <md-field>
+            <md-input></md-input>
+        </md-field>
+        <router-link :to="'/blogs'">
+            <md-button>Search</md-button>
+        </router-link>
+    </div>
+</div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+
+}
 </script>
 
 <style>
-  
+.home-center-container {
+    text-align: center;
+    max-width: 500px;
+    margin: auto;
+    padding: 200px 0;
+}
+
+,
+.home-center {
+    display: inline-block;
+}
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 
 import Home from "./components/Home.vue";
 import BlogPost from "./components/BlogPost.vue";
+import BlogList from "./components/BlogList.vue";
 
 const routes = [
    {path: '/', component: Home},
-   {path: '/blog/:slug', component: BlogPost}
+   {path: '/blogs/:slug', component: BlogPost},
+   {path: '/blogs', component: BlogList}
 
 ];
 
